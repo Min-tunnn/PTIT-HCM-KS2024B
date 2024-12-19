c1450d9b8543917330a8e227f268f67a50ada2fc
Min-tunnn <minhtuan9168@gmail.com>
2024-12-19 23:27:45 +0700

    Add files via upload

--- Dev_C_basic/Session_05/Bai_8.cpp
+++ Dev_C_basic/Session_05/Bai_8.cpp
@@ -1,45 +1,23 @@
 #include <stdio.h>
-#include <math.h> 
-int main() {
-    double a, b, c, delta, x1, x2; 
-    printf("ptrinh bac 2: ax^2 + bx + c = 0\n");
-    printf("Nhap a: "); scanf("%lf", &a);
-    printf("Nhap b: "); scanf("%lf", &b);
-    printf("Nhap c: "); scanf("%lf", &c);
-    if(a==0) 
-	{
-        if(b==0) 
+int main() 
+{
+    int n1, n2, a, b, bcnn;
+    printf("Nhap n1: "); scanf("%d", &n1);
+    printf("Nhap n2: "); scanf("%d", &n2);
+    if (n1 <= 0 || n2 <= 0) {
+        printf("Vui long nhap lai!!\n");
+    } else {
+        a = n1;
+        b = n2;
+        while(b!=0)
 		{
-            if(c==0) printf("Phuong trinh vo so nghiem.");
-			else printf("Phuong trinh vo nghiem.");   
-        } 
-		else 
-		{
-            x1=-c/b;
-            printf("Ptrinh co 1 ng: x = %.2lf\n", x1);
+            int temp=b;
+            b=a%b;
+            a=temp;
         }
-    } 
-	else 
-	{
-        delta = b * b - 4 * a * c;
-        if (delta < 0) {
-            printf("Phuong trinh vo nghiem.\n");
-        } 
-		else if(delta==0) 
-		{
-            x1=-b/(2* a);
-            printf("Ptrinh co ng kep (x1)(x2): %.2lf\n", x1);
-        } 
-		else 
-		{
-            x1=(-b+sqrt(delta))/(2*a);
-            x2=(-b-sqrt(delta))/(2*a);
-            printf("phtrinh co 2 ng phan biet:\n");
-            printf("x1 = %.2lf\n", x1);
-            printf("x2 = %.2lf\n", x2);
-        }
+        bcnn=(n1*n2)/a;
+        printf("BCNN cua %d -> %d la: %d\n", n1, n2, bcnn);
     }
-
     return 0;
 }
 
