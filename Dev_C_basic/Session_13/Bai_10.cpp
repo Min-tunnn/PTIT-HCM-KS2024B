bc7d648ff9e19acb19b5c96d14ca2cc6facdf453
Min-tunnn <minhtuan9168@gmail.com>
2024-12-30 22:16:21 +0700

    Add files via upload

---
+++ Dev_C_basic/Session_13/Bai_10.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#define max 1000
+int main()
+{
+	int choice, n, arr[max];
+	printf("====================***  MENU  ***====================\n");
+	printf("| 1. Nhap phan tu vao mang.                          |\n");
+	printf("| 2. In cac gia tri phan tu                          |\n");
+	printf("| 3. Them phan tu vao vi tri chi dinh.               |\n");
+	printf("| 4. Sua phan tu o vi tri chi dinh.                  |\n");
+	printf("| 5. Xoa phan tu o vi tri chi dinh.                  |\n");
+	printf("| 6. Sap xep cac phan tu trong mang.                 |\n");
+	printf("| 7. Tim kiem cac phan tu nhap vao.                  |\n");
+	printf("| 8. Thoat.                                          |\n");
+	printf("======================================================\n");
+	printf("Lua chon: "); scanf("%d", &choice);
+	switch (choice)
+	{
+		
+	}
+}
