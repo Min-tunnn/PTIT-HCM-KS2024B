18c0935ec66c55627312f1344cf5138adbd4921f
Min-tunnn <minhtuan9168@gmail.com>
2025-03-18 20:33:44 +0700

    Add files via upload

---
+++ Frontend/Session_17/Bai1/index.js
@@ -0,0 +1,11 @@
+const name = prompt("Nhập tên của bạn:");
+const age = prompt("Nhập tuổi của bạn:");
+const date = prompt("Nhập ngày tháng năm sinh:");
+const phone_number = prompt("Nhập số điện thoại:");
+const national = prompt("Nhập quốc tịch:");
+
+console.log(`Tên: ${name}`);
+console.log(`Tuổi: ${age}`);
+console.log(`Ngày sinh: ${date}`);
+console.log(`phone number: ${phone_number}`);
+console.log(`Quốc tịch: ${national}`);
