9b88e8a5fed6e24d82bd15289defac1c3d33a809
Min-tunnn <minhtuan9168@gmail.com>
2025-03-19 20:02:45 +0700

    Add files via upload

--- Frontend/Session_18/Bai6/index.js
+++ Frontend/Session_18/Bai6/index.js
@@ -1,7 +1,7 @@
 let input_year = parseInt(prompt("Nhap nam bat ky: "));
 if(!isNaN(input_year))
 {
-    if(input_year%4==0){
+    if((input_year%4==0 && input_year%100!==0)||(input_year%400===0)){
         document.write("Là năm nhuận");
     }else{
         document.write("Khong phải là năm nhuận");
